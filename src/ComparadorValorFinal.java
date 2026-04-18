d521a79f2f5d514fe160805a9a5360844c7d457f
jalv <araujo.ja.rodrigues@gmail.com>
2026-04-17 22:14:47 -0300

    comparador por valor final - corrigido

--- src/ComparadorValorFinal.java
+++ src/ComparadorValorFinal.java
@@ -9,12 +9,6 @@ public class ComparadorValorFinal implements Comparator<Pedido> {
 
     @Override
     public int compare(Pedido o1, Pedido o2) {
-        if(o1.valorFinal() == o2.valorFinal()) {
-            if(o1.getQuantosProdutos() == o2.getQuantosProdutos())
-                return (o1.hashCode() - o2.hashCode());
-            else
-                return (o1.getQuantosProdutos() - o2.getQuantosProdutos());
-        }
         return (int) (o1.valorFinal() - o2.valorFinal());
     }
 }
